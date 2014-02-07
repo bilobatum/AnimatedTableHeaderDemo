c466f1e229cf68883c48f61f9e762fb3aa82a2d1
Andrew Black <andrew@uw.edu>
2014-02-07 14:36:23 -0800

    Initial Commit

---
+++ TableHeaderDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableHeaderDemo
+//
+//  Created by Andrew Black on 2/7/14.
+//  Copyright (c) 2014 Andrew Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TableHeaderDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TableHeaderDemo
+//
+//  Created by Andrew Black on 2/7/14.
+//  Copyright (c) 2014 Andrew Black. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
